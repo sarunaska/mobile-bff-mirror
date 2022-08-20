1bcf0a0b90e7dff2a830626c1b6772023e18b9a5
Sarunas K <sarunas.kavaliauskas@softeta.com>
2022-08-20 09:58:22 +0300

    initial

--- Tests.API/PrivateAccounts/GetAccounts.cs
+++ Tests.API/PrivateAccounts/GetAccounts.cs
@@ -10,18 +10,21 @@ using Tests.ResponseProviders;
 using Xunit.Abstractions;
 using Match = PactNet.Matchers.Match;
 using Constants = MobileBff.Models.Constants;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Tests.API.PrivateAccounts
 {
     [Collection("API Test Collection")]
     public class GetAccounts : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly ITestOutputHelper _testOutputHelper;
         private readonly HttpClient _client;
         private readonly IPactBuilderV3 _pactBuilder;
         private readonly int _pactMockPort = 9090;
             
-        public GetAccounts(WebApplicationFactory<Program> factory, ITestOutputHelper output)
+        public GetAccounts(WebApplicationFactory<Program> factory, ITestOutputHelper output, ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = output;
             factory = factory.WithWebHostBuilder(builder =>
             {
                 builder.ConfigureAppConfiguration((context, configBuilder) =>
@@ -74,6 +77,7 @@ namespace Tests.API.PrivateAccounts
             await _pactBuilder.VerifyAsync(async _ =>
             {
                 var accountsResponse = await _client.GetFromJsonAsync<AccountsResult>("/corp/accounts");
+                _testOutputHelper.WriteLine(JsonSerializer.Serialize(accountsResponse));
                 Assert.NotNull(accountsResponse);
                 Assert.NotEmpty(accountsResponse!.AccountGroups);
                 Assert.NotEmpty(accountsResponse.AccountGroups.ToList()[0].Accounts);
